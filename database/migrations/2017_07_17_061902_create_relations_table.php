487931370b223ac2807d162745b4d5167b78a05d
Pho Duc Dat <phoducdat@gmail.com>
2017-07-24 17:37:22 +0700

    search

--- database/migrations/2017_07_17_061902_create_relations_table.php
+++ database/migrations/2017_07_17_061902_create_relations_table.php
@@ -53,6 +53,46 @@ class CreateRelationsTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('CASCADE')->onUpdate('CASCADE');
             $table->foreign('province_id')->references('id')->on('provinces')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('follower_id')->references('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('following_id')->references('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('user_gallerys', function ($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('services', function ($table) {
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('province_id')->references('id')->on('provinces')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('service_gallerys', function ($table) {
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('comments', function ($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('plans', function ($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('plan_locations', function ($table) {
+            $table->foreign('province_id')->references('id')->on('provinces')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('plan_id')->references('id')->on('plans')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('plan_details', function ($table) {
+            $table->foreign('plan_id')->references('id')->on('plans')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('requested_services', function ($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('province_id')->references('id')->on('provinces')->onDelete('CASCADE')->onUpdate('CASCADE');
         });
 
     }
