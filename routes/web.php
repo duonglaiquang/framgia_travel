52c8e65a602bcad5214ec99b384eb248fd77aa6b
Pho Duc Dat <phoducdat@gmail.com>
2017-09-05 17:11:04 +0700

    requestBeta

--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,4 @@ Route::group(['prefix' => '/action', 'middleware' => ['IsUser', 'auth']], functi
 });
 
 Route::get('/search{id}{id1}', ['as' => 'PFsearch', 'uses' => 'PagesController@PFsearch']);
+Route::get('/service/{province_id}/{type_id}', ['as' => 'select-ajax', 'uses' => 'AjaxController@selectAjax']);
